4fd1eaffebe280e91253e5ae5ba2d1ffd49a70b4
Ozodbek <email@misol.com>
2025-12-16 23:27:19 +0500

    Project

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Request;
+
+class Cors
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+
+        $response->headers->set('Access-Control-Allow-Origin', 'https://onlineadminmanager.netlify.app/');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+        return $response;
+    }
+}
